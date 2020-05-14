e861d140b4700aa0065505631e720c456f1fe113
jaskirat1313 <jaskiratsingh568@gmail.com>
2020-05-14 23:48:44 +0530

    week1

---
+++ lecture5 Expressions and Interpolation/app.js
@@ -0,0 +1,21 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.name = "Jaskirat";
+  $scope.stateOfBeing = "hungry";
+
+  $scope.sayMessage = function () {
+    return "Jaskirat likes to eat healthy snacks at night!";
+  };
+
+  $scope.feedJaskirat = function () {
+    $scope.stateOfBeing = "fed";
+  };
+}
+
+})();
